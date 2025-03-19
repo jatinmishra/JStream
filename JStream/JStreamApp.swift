b4bf82d5c868366f39a2fd7da7ab0bb676d3b829
Jatin Mishra <jatinmishra27@gmail.com>
2025-03-19 12:57:06 +0530

    Initial Commit

---
+++ JStream/JStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  JStreamApp.swift
+//  JStream
+//
+//  Created by Jatin on 19/03/25.
+//
+
+import SwiftUI
+
+@main
+struct JStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
